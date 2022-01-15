6de176d47750af668edc67af789cf5d57e915d17
surbhit <surbhitagrawal716@gmail.com>
2022-01-15 13:38:36 +0530

    Initial template

---
+++ src/redux/blockchain/blockchainActions.js
@@ -0,0 +1,82 @@
+// constants
+import Web3 from "web3";
+import LipToken from "../../contracts/LipToken.json";
+// log
+import { fetchData } from "../data/dataActions";
+
+const connectRequest = () => {
+  return {
+    type: "CONNECTION_REQUEST",
+  };
+};
+
+const connectSuccess = (payload) => {
+  return {
+    type: "CONNECTION_SUCCESS",
+    payload: payload,
+  };
+};
+
+const connectFailed = (payload) => {
+  return {
+    type: "CONNECTION_FAILED",
+    payload: payload,
+  };
+};
+
+const updateAccountRequest = (payload) => {
+  return {
+    type: "UPDATE_ACCOUNT",
+    payload: payload,
+  };
+};
+
+export const connect = () => {
+  return async (dispatch) => {
+    dispatch(connectRequest());
+    if (window.ethereum) {
+      let web3 = new Web3(window.ethereum);
+      try {
+        
+        if (true) {
+
+          const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+          const lipToken = new web3.eth.Contract(
+            LipToken,
+            "0x4AFfa4c6e3dBFF8D5cd43ce688B2f2f2eeA64B3f"
+          );
+          console.log(accounts)
+          console.log(lipToken)
+          dispatch(
+            connectSuccess({
+              account: accounts[0],
+              lipToken: lipToken,
+              web3: web3,
+            })
+          );
+          // Add listeners start
+          window.ethereum.on("accountsChanged", (accounts) => {
+            dispatch(updateAccount(accounts[0]));
+          });
+          window.ethereum.on("chainChanged", () => {
+            window.location.reload();
+          });
+          // Add listeners end
+        } else {
+          dispatch(connectFailed("Change network to Polygon."));
+        }
+      } catch (err) {
+        dispatch(connectFailed("Something went wrong."));
+      }
+    } else {
+      dispatch(connectFailed("Install Metamask."));
+    }
+  };
+};
+
+export const updateAccount = (account) => {
+  return async (dispatch) => {
+    dispatch(updateAccountRequest({ account: account }));
+    dispatch(fetchData(account));
+  };
+};
