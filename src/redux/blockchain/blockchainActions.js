99685aff91a325a7d82d6c464c82bc1db6476b06
surbhit14 <surbhitagrawal716@gmail.com>
2022-04-07 18:37:52 +0530

    debug

--- src/redux/blockchain/blockchainActions.js
+++ src/redux/blockchain/blockchainActions.js
@@ -35,8 +35,7 @@ export const connect = () => {
   return async (dispatch) => {
     dispatch(connectRequest());
     if (window.ethereum) {
-      // let web3 = new Web3(window.ethereum);
-      const web3 = new Web3('https://rpctest.meter.io');
+      let web3 = new Web3(window.ethereum);
       try {
           const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
           const godToken = new web3.eth.Contract(
